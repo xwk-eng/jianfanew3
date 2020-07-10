c49263db015142473afb2be56e1fdd770dd448f9
yuebuqun <1411681860@qq.com>
2020-07-11 00:15:41 +0800

    bixiejianfa1

---
+++ src/main/java/com/atguigu/huashan/jianfanew1.java
@@ -0,0 +1,17 @@
+package com.atguigu.huashan;
+
+/**
+ * @author xwkstart
+ * @create 2020-07-11 0:14
+ */
+public class jianfanew1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("huashan--------jian");
+        System.out.println("huashan--------jian");
+        System.out.println("huashan--------jian");
+        System.out.println("huashan--------jian");
+
+    }
+}

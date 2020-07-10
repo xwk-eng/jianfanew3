638f61160cf5a24402b3c6f3c80e41186864fb78
xwk-eng <1411681860@qq.com>
2020-07-11 00:18:38 +0800

    bixiejianfa1

--- src/main/java/com/atguigu/huashan/jianfanew1.java
+++ src/main/java/com/atguigu/huashan/jianfanew1.java
@@ -2,16 +2,7 @@ package com.atguigu.huashan;
 
 /**
  * @author xwkstart
- * @create 2020-07-11 0:14
+ * @create 2020-07-11 0:16
  */
 public class jianfanew1 {
-
-    public static void main(String[] args) {
-
-        System.out.println("huashan--------jian");
-        System.out.println("huashan--------jian");
-        System.out.println("huashan--------jian");
-        System.out.println("huashan--------jian");
-
-    }
 }

bff81156046c083f95170a0634abc01b60a09eb3
xwk-eng <1411681860@qq.com>
2020-07-11 00:19:32 +0800

    bixiejianfa2

--- src/main/java/com/atguigu/huashan/jianfanew1.java
+++ src/main/java/com/atguigu/huashan/jianfanew1.java
@@ -5,4 +5,14 @@ package com.atguigu.huashan;
  * @create 2020-07-11 0:16
  */
 public class jianfanew1 {
+    public static void main(String[] args) {
+
+        System.out.println("huashan=====jian");
+        System.out.println("huashan=====jian");
+        System.out.println("huashan=====jian");
+        System.out.println("huashan=====jian");
+
+    }
+
+
 }
